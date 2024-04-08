0040fc177eaf03c01b70f63bcbaf3f8c41761345
viajiefan <viajiefan@gmail.com>
2024-04-08 23:29:36 +0900

    add filter en version

--- data_management/preprocessing/filtering/filter_divided_en_json.py
+++ data_management/preprocessing/filtering/filter_divided_en_json.py
@@ -1,6 +1,6 @@
 # 分割された複数のjsonl ファイルに対して，1jsonl--1thread で復数のjsonlファイルを処理する
 # jsonlの名前の並び や, 処理するidxの範囲を指定すること 
-# usage: python filter_divided_ja_json.py --input_dir /path/to/input_dir --output_dir /path/to/output_dir
+# usage: python filter_divided_en_json.py --input_dir /path/to/input_dir --output_dir /path/to/output_dir
 
 from datetime import datetime
 import json
@@ -17,7 +17,7 @@ def __readlines(input_file: str):
     with open(input_file) as fp:
         return fp.readlines()
 
-def process_json_lines_ja(inputs):
+def process_json_lines_en(inputs):
     print(f"process json filter inputs: {inputs}")
     json_filename, input_dir, output_dir = inputs
     lines = __readlines(input_dir + json_filename)
@@ -27,15 +27,15 @@ def process_json_lines_ja(inputs):
         document_filters.JSONLoader(),
         document_filters.DocumentNormalizer(),
         document_filters.DiscardBBSComments(),
-        document_filters.DiscardAds(),
-        document_filters.DiscardDiscriminationContentJa(),
-        custom_document_filters.DiscardAdultContentJa(),
-        custom_tokenization.NewLineSentenceTokenizer(),
-        custom_token_filters.RemoveOneword(),
-        custom_tokenization.MergeTokens(delimiter="\n"),
-        custom_tokenization.WakatiTokenizer(),
-        custom_token_filters.RemoveDate(),
-        tokenization.MergeTokens(),
+
+        custom_document_filters.DiscardAdultContentEn(),
+        custom_document_filters.DiscardBadWordEn(),
+        # custom_tokenization.NewLineSentenceTokenizer(),
+        # custom_token_filters.RemoveOneword(),
+        # custom_tokenization.MergeTokens(delimiter="\n"),
+        # custom_tokenization.WakatiTokenizer(),
+        # custom_token_filters.RemoveDate(),
+        # tokenization.MergeTokens(),
         document_filters.MaskPersonalInformation(),
         document_filters.JSONDumper(dump_reason=True),
     ])
@@ -55,8 +55,8 @@ def process_json_lines_ja(inputs):
         writer.write(json.dumps(cleaner.statistics, ensure_ascii=False) + "\n")
 
 
-def mc4_ja_main():
-    """ mc4-ja mc4-ja-00X.jsonl についてフィルタリングする """
+def mc4_en_main():
+    """ mc4-en  についてフィルタリングする """
     parser = argparse.ArgumentParser(description='Process some documents.')
     parser.add_argument('--input_dir', type=str,
                         help='The input directory containing documents to process', required=True)
